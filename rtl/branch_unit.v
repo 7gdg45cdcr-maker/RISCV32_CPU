bd3faabe7a969fc6ad4b3b41495215af9acbb3db
Abitha_Chokka <7gdg45cdcr@privaterelay.appleid.com>
2026-07-10 17:11:32 +0530

    Implement branch decision unit in Verilog

---
+++ rtl/branch_unit.v
@@ -0,0 +1,38 @@
+//==============================================================
+// Branch Decision Unit
+//==============================================================
+
+module branch_unit(
+
+    input [31:0] rs1,
+    input [31:0] rs2,
+    input [2:0] funct3,
+
+    output reg branch_taken
+
+);
+
+always @(*) begin
+
+    case(funct3)
+
+        3'b000:
+            branch_taken = (rs1==rs2);
+
+        3'b001:
+            branch_taken = (rs1!=rs2);
+
+        3'b100:
+            branch_taken = ($signed(rs1)<$signed(rs2));
+
+        3'b101:
+            branch_taken = ($signed(rs1)>=$signed(rs2));
+
+        default:
+            branch_taken = 1'b0;
+
+    endcase
+
+end
+
+endmodule
