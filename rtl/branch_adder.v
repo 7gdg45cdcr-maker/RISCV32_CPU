38a306a7b4d909ffeea26612e2a4be8462edd212
Abitha_Chokka <7gdg45cdcr@privaterelay.appleid.com>
2026-07-10 17:12:04 +0530

    Add branch_adder module for address calculation

---
+++ rtl/branch_adder.v
@@ -0,0 +1,16 @@
+//==============================================================
+// Branch Address Generator
+//==============================================================
+
+module branch_adder(
+
+    input [31:0] pc,
+    input [31:0] immediate,
+
+    output [31:0] branch_address
+
+);
+
+assign branch_address = pc + immediate;
+
+endmodule
